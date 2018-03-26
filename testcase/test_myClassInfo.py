a1dbbd46a82835065225e4fac084ff5458652991
liyao <muzi379178228@qq.com>
2018-03-26 12:58:43 +0800

    myClassInfo_case 修改

--- testcase/test_myClassInfo.py
+++ testcase/test_myClassInfo.py
@@ -11,7 +11,24 @@ class MyClassInfo(PubTest):
 
     }
 
-    def test_MyClassInfo(self):
+    def test_MyClassInfo_classInfo_case(self):
         code = requests.request("POST", url=PubTest.myClassInfo, headers=PubTest.headers, data=json.dumps(self.play))
         self.assertIn('classInfo', code.json())
 
+    def test_MyClassInfo_page_case(self):
+        code = requests.request("POST", url=PubTest.myClassInfo, headers=PubTest.headers, data=json.dumps(self.play))
+        print(code.json())
+        self.assertIn('page', code.json())
+
+    def test_MyClassInfo_msg_case(self):
+
+        code = requests.request("POST", url=PubTest.myClassInfo, headers=PubTest.headers, data=json.dumps(self.play))
+        ss = json.loads(code.text)
+        self.assertEquals(ss['msg'], 'Ok')
+
+    def test_MyClassInfo_tokenError_case(self):
+        re = deepcopy(self.play)
+        re['token'] = None
+        code = requests.request("POST", url=PubTest.myClassInfo, headers=PubTest.headers, data=json.dumps(re))
+        ss = json.loads(code.text)
+        self.assertEquals(ss['status'],0)
